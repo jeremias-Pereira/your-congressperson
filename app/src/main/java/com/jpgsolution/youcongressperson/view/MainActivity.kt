703d1ea7c26e9bf41058d0f8aa032971d4231c34
jeremias Pereira <jappsolution@gmail.com>
2022-04-12 16:05:20 -0300

    update UI

--- app/src/main/java/com/jpgsolution/youcongressperson/view/MainActivity.kt
+++ app/src/main/java/com/jpgsolution/youcongressperson/view/MainActivity.kt
@@ -1,42 +1,89 @@
-package com.jpgsolution.youcongressperson
+package com.jpgsolution.youcongressperson.view
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.Window
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.jpgsolution.youcongressperson.adapter.Adapter
+import com.jpgsolution.youcongressperson.R
 import com.jpgsolution.youcongressperson.databinding.ActivityMainBinding
-import com.jpgsolution.youcongressperson.model.CongressPerson
-import com.jpgsolution.youcongressperson.repository.Repository
+import com.jpgsolution.youcongressperson.di.DaggerDataSourceComponent
+import com.jpgsolution.youcongressperson.model.datamodel.CongressPerson
+import com.jpgsolution.youcongressperson.usecase.home.Home
+import com.jpgsolution.youcongressperson.usecase.home.presentation.HomePresenter
+import com.jpgsolution.youcongressperson.view.adapter.Adapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Home.ViewHome{
 
     private lateinit var binding: ActivityMainBinding
     private val dataList: MutableList<CongressPerson> = mutableListOf()
     private lateinit var adapter: Adapter
-    private  val repository = Repository()
-    @SuppressLint("NotifyDataSetChanged")
+    private lateinit var presenter: HomePresenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        adapter = Adapter(dataList)
-        repository.congressPerson.observe({lifecycle}){listCongressPerson ->
-            listCongressPerson?.let {
-                dataList.clear()
-                dataList.addAll(it)
-                adapter.notifyDataSetChanged()
-            }
-        }
-        repository.getCongressPerson()
-        initRecycler()
+        setUpToolbar()
+        setUpPresenter()
+        setDataInRecyclerView()
+        actionRefresh()
     }
 
-    private fun initRecycler(){
-        binding.recycler.layoutManager = LinearLayoutManager(applicationContext,LinearLayoutManager.VERTICAL,false)
+    override fun onStart() {
+        super.onStart()
+        presenter.checkNetwork()
+    }
+
+    private fun setUpToolbar(){
+//        binding.appBarLayout.toolbar.title = "Seus Parlamentares"
+        setSupportActionBar(binding.appBarLayout.toolbar)
+    }
+
+    private fun setUpPresenter(){
+        val repository = DaggerDataSourceComponent
+            .builder()
+            .build()
+            .remoteDataSource()
+        presenter = HomePresenter(this, repository, applicationContext)
+    }
+
+    private fun setDataInRecyclerView(){
+        adapter = Adapter(dataList, presenter)
         binding.recycler.setHasFixedSize(true)
         binding.recycler.adapter = adapter
     }
 
+    override fun showNetWorkError(message: String) {
+        Log.d("STATE", message)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun showCongressPerson(congressPerson: List<CongressPerson>) {
+        congressPerson.let {
+            dataList.clear()
+            dataList.addAll(it)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.onStop()
+    }
+
+    private fun actionRefresh(){
+        binding.refresh.setOnRefreshListener {
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.refresh.isRefreshing = false
+                presenter.getCongressPerson()
+            },5000)
+        }
+    }
 }
